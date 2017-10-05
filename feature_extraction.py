4d4314ba95fb917639de96d6d8347caf05963be1
amschwinn <schwinnam@gmail.com>
2017-10-05 14:19:31 +0200

    Added dictionary

--- feature_extraction.py
+++ feature_extraction.py
@@ -12,8 +12,9 @@ Oussama Bouldjedri
 
 October 3, 2017
 """
-
+#%%
 import numpy as np
+import pandas as pd
 import cv2
 import os
 import time
@@ -28,42 +29,51 @@ dsc_sift = []
 #images = []
 
 #Set directory path for image folders
-folder="C:/Users/schwi/Google Drive/MLDM/Computer Vision Project/Data/trainVal_VOC2007/JPEGImages"    
+folder="C:/Users/schwi/Google Drive/MLDM/Computer Vision Project/Data/obj_extracted/"    
 
+#%%
 #Track run time
 start=time.time()
 
 #Iterate through images extracting key poins and descriptors
 for filename in os.listdir(folder):
-    #Import image
-    img = cv2.imread(os.path.join(folder,filename))
+    if filename != 'desktop.ini':
+        #Import image
+        img = cv2.imread(os.path.join(folder,filename))
     
-    #Convert to grayscale
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        #Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        #load feature detection algorithms
+        brisk=cv2.BRISK_create()
+        sift = cv2.xfeatures2d.SIFT_create()
+  
+        #Run feature detection algorithm
+        k_brisk = brisk.detect(gray,None)
+        k_sift = sift.detect(gray,None)
+       
+        #Computer keypoints and descriptors
+        k_brisk,d_brisk = brisk.compute(gray,k_brisk)
+        k_sift,d_sift = brisk.compute(gray,k_sift)
+        if d_brisk == None:
+            print(filename)
+            print(filename[4:filename.index('_',5)])
+        #Store the keypoints and descriptors   
+        dsc_brisk.append({'obj':filename[4:filename.index('_',5)],
+            'brisk':d_brisk})
+        dsc_sift.append({'obj':filename[4:filename.index('_',5)],
+            'sift':d_sift})
     
-    #load feature detection algorithms
-    brisk=cv2.BRISK_create()
-    sift = cv2.xfeatures2d.SIFT_create()
+        #Draw and show kepoints on image
+        #img2 = cv2.drawKeypoints(img,kp,img,color=(0,255,0), flags=0)
+        #plt.imshow(img2),plt.show()
 
-    #Run feature detection algorithm
-    k_brisk = brisk.detect(gray,None)
-    k_sift = sift.detect(gray,None)
-    
-    #Computer keypoints and descriptors
-    k_brisk,d_brisk = brisk.compute(gray,k_brisk)
-    k_sift,d_sift = brisk.compute(gray,k_sift)
-
-    #Store the keypoints and descriptors
-    kp_brisk.append(k_brisk)     
-    dsc_brisk.append(d_brisk)
-    kp_sift.append(k_sift)     
-    dsc_sift.append(d_sift)
-
-    #Draw and show kepoints on image
-    #img2 = cv2.drawKeypoints(img,kp,img,color=(0,255,0), flags=0)
-    #plt.imshow(img2),plt.show()
-   
 #End timer and show run time
 end=time.time()
 print(end - start)
 
+#%%
+check = dsc_brisk[1]['brisk']
+check2 = dsc_brisk[0]['brisk']
+
+check3 = numpy.stack(check,check2)
