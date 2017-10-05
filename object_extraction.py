2ce757302522b0c3cd0eebee774d135952fff4ee
Jérémie BLANCHARD <jeremieblanchard@free.fr>
2017-10-05 12:55:48 +0200

    Generalize the objects extraction

--- object_extraction.py
+++ object_extraction.py
@@ -7,6 +7,7 @@ Objects extraction from a given image
 
 import pymysql
 import pymysql.cursors
+import os
 
 import cv2
 #import numpy as np
@@ -21,12 +22,16 @@ sql_get_info_object = "SELECT * FROM `objects` WHERE `num_img`=%s "
 path = "C:/Users/jerem/Desktop/M2/CV/VOCdevkit/VOC2007/JPEGImages/"
 image = "009822.jpg" #Name of the image we want to rextract objects from
 
-img = cv2.imread(path+image,3) #We open the image
 
-with conn.cursor() as cursor:
-    cursor.execute(sql_get_info_object,image) #We execute our SQL request
-    conn.commit()
-    for row in cursor: #Extract every object and save them as images named as follow : obj_"ID_Object"_"num-img".png
-        obj = img[row[7]:row[9],row[6]:row[8]]
-        cv2.imwrite('obj_'+str(row[0])+'_'+row[1]+'.png',obj)
+for filename in os.listdir(path):
+    
+    #Import image
+    img = cv2.imread(os.path.join(path,filename))
+    
+    with conn.cursor() as cursor:
+        cursor.execute(sql_get_info_object,filename) #We execute our SQL request
+        conn.commit()
+        for row in cursor: #Extract every object and save them as images named as follow : obj_"ID_Object"_"num-img".png
+            obj = img[row[7]:row[9],row[6]:row[8]]
+            cv2.imwrite('obj/obj_'+str(row[0])+'_'+row[1]+'.png',obj)
 
