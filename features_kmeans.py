cb4b0dbbfd0fc9c38fd61a4d46be34aeed0f100d
amschwinn <schwinnam@gmail.com>
2017-10-25 15:20:05 +0200

    Combined all object kmeans centroids into 1 total kmeans Output ready for SVM step

--- features_kmeans.py
+++ features_kmeans.py
@@ -20,56 +20,170 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 import pandas as pd
+import os
 
 #%%
 
 # Connect to the database.
-conn = pymysql.connect(db='images_db', user='root', passwd='', host='localhost')
+conn = pymysql.connect(db='imagesdb', user='root', passwd='', host='localhost')
+#%%
+sql_get_descriptors = "SELECT * FROM desc_obj d INNER JOIN objects o \
+    ON  d.id_obj = o.ID_object;"
 
-sql_get_descriptors = "SELECT * FROM `desc_obj`"
-
+#%%
 with conn.cursor() as cursor:
     cursor.execute(sql_get_descriptors) #We execute our SQL request
     conn.commit()
     
     it = 0
     list_conca=[]
-    
+    obj_name=[]
+    obj_id=[]
+
     for row in cursor:
         if it < 1000:
             json_desc = json.loads(row[2])
             list_conca = list_conca + json_desc['sift']
+            print('check1')
+            obj_name = obj_name + np.repeat(row[5],len(
+                    json_desc['sift'])).tolist()
+            print('check2')
+            obj_id = obj_id + np.repeat([json_desc['obj']],len(
+                    json_desc['sift'])).tolist()
             print("Passage n°",it)
             it+=1
         else:
            break
-#%%
-#Move list of arrays to 2d df
-df_conca = pd.DataFrame(list_conca)
 
+
+#Combine into df
+#df_conca = pd.concat([pd.Series(list_conca),pd.Series(obj_name),
+#                      pd.Series(obj_id)],axis=1)
+
+#%%
+#Transform into df for pca
+pca_conca = pd.DataFrame(list_conca)
+
+#Create table to store PCA values
+pca_results = pd.DataFrame(columns=['evr','n_comp'])
+
+#%%
+'''
 #load pca and run
 for n in range(64):
     pca = PCA(n_components=n)
-    pca.fit(df_conca)
+    pca.fit(pca_conca)
+    df = pd.DataFrame.from_dict({"evr":[pca.explained_variance_ratio_.sum()],
+            "n_comp":[n]})
+    pca_results = pd.concat([pca_results,df])
     print(n)
     print(pca.explained_variance_ratio_.sum())
+'''    
+#%%
+#Use desired PCA for actual PCA matrix
+n_comp = 50
+pca = PCA(n_components=n_comp)
+pca.fit(pca_conca)
+print(pca.explained_variance_ratio_.sum())
+pca_conca = pd.DataFrame(pca.transform(pca_conca))
+#pca_conca['obj_name'] = pd.Series(obj_name)
 
 #%%
-    distortions = []
-    
-    for k in range(10,15):
+'''
+#Table to store kmean reults
+kmeans_results = pd.DataFrame(columns=['k','obj_name','distortion','run_time'])
+distortions = []
+centroids = {}
+
+#Run kmeans for each object
+for obj in pca_conca.obj_name.unique():
+    #Only cluster descriptors from this object
+    df = pca_conca[pca_conca.obj_name==obj].drop(['obj_name'],axis=1)
+    print("subset taken")
+    #iterate through number of k
+    #for k in range(10,100,10):
+    for k in [50]:
         #Track run time
         start=time.time()
-        kmeans = KMeans(n_clusters=k).fit(list_conca)
-        distortions.append(sum(np.min(cdist(list_conca, kmeans.cluster_centers_, 'euclidean'),axis=1))/64)
-        #End timer and show run time
+        kmeans = KMeans(n_clusters=k).fit(df)
         end=time.time()
-        print("Done for nb_cluster=",k," - In ", end - start)
-        
-     
-    # Plot the elbow
-    plt.plot(range(10,15), distortions, 'bx-')
+        print(obj + ':complete')
+        print(end-start)
+        results_df = pd.DataFrame.from_dict({"k":[k],"run_time":[(end-start)],
+            "distortion":[sum(np.min(cdist(df, kmeans.cluster_centers_, 
+            'euclidean'),axis=1))/n_comp],"obj_name":[obj]})
+        kmeans_results = pd.concat([kmeans_results,results_df])
+        centroids[obj] = kmeans.cluster_centers_
+
+
+
+#Total time
+print(kmeans_results.run_time.sum())
+
+#Write results files to csv's
+pca_results.to_csv('pca_results.csv')
+kmeans_results.to_csv('kmeans_results.csv')
+
+#Centroids
+for keys, values in centroids.items():
+    file = 'kmeans_centroids' + str(keys)+'.csv'
+    df = pd.DataFrame(values)
+    df.to_csv(file)
+
+kmeans_results = pd.read_csv('kmeans_results.csv')    
+  
+# Plot the elbow
+for i in kmeans_results.obj_name.unique():
+    k = kmeans_results[kmeans_results.obj_name == i].k
+    distortions = kmeans_results[kmeans_results.obj_name == i].distortion
+    plt.plot(k, distortions, 'bx-')
     plt.xlabel('k')
     plt.ylabel('Distortion')
-    plt.title('The Elbow Method showing the optimal k')
+    plt.title(i)
     plt.show()
+'''    
+#%%
+#Load centroid results for each object
+folder="C:/Users/schwi/Google Drive/MLDM/Computer Vision Project/results"
+centroids = {}
+for filename in os.listdir(folder):
+    if filename != 'desktop.ini':
+        #Import image
+        cent = pd.DataFrame.from_csv(filename)
+        obj = filename[16:].replace('.csv','')
+        centroids[obj] = cent
+
+#Combine all object centroids into one df
+centroid_total = pd.DataFrame(columns=list(centroids['aeroplane']))
+for keys, values in centroids.items():
+    centroid_total = pd.concat([centroid_total,values])
+    
+#Create k-means model with the total centroids
+kmeans_total = KMeans(init=centroid_total,n_clusters=len(centroid_total),
+                      n_init=1).fit(centroid_total)
+
+#Predict on images
+centroids_total = kmeans_total.predict(pca_conca)
+
+#Combine results to feed to SVM
+kmeans_clusters = pd.concat([pd.Series(centroids_total),pd.Series(obj_id),
+                             pd.Series(obj_name)],axis=1)
+kmeans_clusters = kmeans_clusters.rename(columns={0:'cluster',1:'obj_id',
+                                                  2:'obj_name'})
+    
+#Get count of each cluster for each object
+cluster_pivot = kmeans_clusters.groupby(['obj_id','cluster'],
+                                        as_index=False).count()
+cluster_pivot = cluster_pivot.rename(columns={'obj_name': 'count'})
+
+#Pivot df to have each cluster as col feature and each object as observation
+cluster_pivot = cluster_pivot.pivot(index='obj_id',columns='cluster',
+                                    values='count')
+
+#Get labels ready to join
+labels = kmeans_clusters[['obj_id','obj_name']].drop_duplicates().set_index(['obj_id'])
+
+#join labels
+cluster_pivot = cluster_pivot.join(labels, how='inner')
+
+#Read for SVM
