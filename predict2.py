8528f18f9e43e76deef90e9537f78f8dd4977cde
Oussamab21 <oussamabouldjedri2@gmail.com>
2017-12-25 05:20:43 +0100

    Signed-off-by: Oussamab21 <oussamabouldjedri2@gmail.com> this file is for testing using all the images in the testing folder

---
+++ predict2.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 21:12:27 2017
+
+@author: dell1
+"""
+
+import sys
+import argparse
+import numpy as np
+from PIL import Image
+import requests
+from io import BytesIO
+import matplotlib.pyplot as plt
+import cv2
+
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.applications.inception_v3 import preprocess_input
+import os  
+
+target_size = (229, 229) #fixed size for InceptionV3 architecture
+Classes=["areoplane","bycicle","bird","boat","bottle","bus","car","cat","chair",
+         "cow","digningtable","dog","horse","motorbike","person","pottedplant","sheep"
+         "sofa","train","TVmonitor"]
+
+def predict(model, img, target_size):
+  """Run model prediction on image
+  Args:
+    model: keras model
+    img: PIL format image
+    target_size: (w,h) tuple
+  Returns:
+    list of predicted labels and their probabilities
+  """
+  if img.size != target_size:
+    img = img.resize(target_size)
+
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+  x = preprocess_input(x)
+  preds = model.predict(x)
+  return preds[0]
+
+
+def plot_preds(image, preds):
+  """Displays image and the top-n predicted probabilities in a bar graph
+  Args:
+    image: PIL image
+    preds: list of predicted labels and their probabilities
+  """
+  plt.imshow(image)
+  plt.axis('off')
+
+  plt.figure()
+  labels = ("cat", "dog")
+  plt.barh([0, 1], preds, alpha=0.5)
+  plt.yticks([0, 1], labels)
+  plt.xlabel('Probability')
+  plt.xlim(0,1.01)
+  plt.tight_layout()
+  plt.show()
+
+
+if __name__=="__main__":
+  a = argparse.ArgumentParser()
+  a.add_argument("--image", help="path to image folder")
+  a.add_argument("--image_url", help="url to image")
+  a.add_argument("--model")
+  args = a.parse_args()
+
+  if args.image is None and args.image_url is None:
+    a.print_help()
+    sys.exit(1)
+
+  model = load_model(args.model)
+  if args.image is not None:
+    for subdirs, dirs, files in os.walk(args.image):  
+  
+     for file in files:  
+        #if file == '*.shp':  
+      print("file is ",file)
+      pat=args.image+'/'+file
+      print("path is ",pat)
+      #img = Image.open(args.image)
+      img = Image.open(pat)
+      print("foo")
+      preds = predict(model, img, target_size)
+      print(preds)
+      #plot_preds(img, preds)
+      ind=np.argmax(preds)
+      
+      #print("there is a probability of ",preds[ind]*100)
+      #print(" this object is from the class",Classes[ind])
+      print()
+    '''
+    orig = cv2.imread(args.image)
+   #(imagenetID, label, prob) =
+    label=Classes[ind]
+    prob=preds[ind]*100
+    #cv2.putText(orig, "Label: {}, {:.2f}%".format(label, prob),
+	#(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+    #cv2.imshow("Classification", orig)
+    #cv2.waitKey(0)
+    '''
+  if args.image_url is not None:
+    response = requests.get(args.image_url)
+    img = Image.open(BytesIO(response.content))
+    preds = predict(model, img, target_size)
+    plot_preds(img, preds)
+
